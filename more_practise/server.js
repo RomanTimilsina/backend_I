f665343f7b4ae489ac0a2abbbe63daba91f1eceb
Roman <romantimilsina@gmail.com>
2023-02-27 09:16:26 +0545

    rendering_html

--- more_practise/server.js
+++ more_practise/server.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", 'ejs')
+
 app.get('/', (req,res) => {
-  res.download("server.js")
-  
+  res.render('index', { text:'World'} )
 })
 
 app.listen(3000)
