260ca3326ed8fb287c9295e9b2cff291788f62c5
Roman <romantimilsina@gmail.com>
2023-02-27 13:21:10 +0545

    rendering_static_files

--- more_practise/server.js
+++ more_practise/server.js
@@ -2,14 +2,18 @@ const express = require("express")
 const app = express()
 
 app.set("view engine", 'ejs')
+app.use(express.static("public"))
 
-app.get('/', (req,res) => {
-  res.render('index', { text:'World'} )
-})
+//app.get('/', (req,res) => {
+  //res.render('index', { text:'World'} )
+//})
 
 const userRouter = require("./routes/users.js")
 app.use("/users", userRouter)
 
 
-
 app.listen(3000)
+
+
+
+
